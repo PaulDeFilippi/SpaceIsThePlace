a385a3db42c26d00f13928671f62e6baf2b9c3ed
Paul Defilippi <BlindFaith@pauls-imac.local>
2017-02-03 00:31:54 -0500

    Interactive Story mode Complete

--- Interactivestory/ViewController.swift
+++ Interactivestory/ViewController.swift
@@ -12,14 +12,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+//        let story = Page(story: .TouchDown)
+//        story.firstChoice = (title: "some Title", page: Page(story: .Droid))
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
